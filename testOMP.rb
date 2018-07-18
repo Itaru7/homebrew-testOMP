a646c9fecddd195a79c0e69aff4b238a355ef307
Itaru7 <ir172178@gmail.com>
2018-07-18 14:31:54 -0700

    change to constant

--- testOMP.rb
+++ testOMP.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class testOMP < Formula
+class TestOMP < Formula
   homepage "https://github.com/Itaru7/homebrew-testOMP"
   url "https://github.com/Itaru7/homebrew-testOMP/archive/v1.2.tar.gz"
   sha256 "18b6a72a9f3e952c4b4a6f58eb5995d80f94dcc09d433865b38f8816d2dc1f53"

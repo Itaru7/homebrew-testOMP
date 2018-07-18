687a3d09cd0803e3dda22085938995c7ca247f12
Itaru7 <ir172178@gmail.com>
2018-07-18 14:40:37 -0700

    changed

--- testOMP.rb
+++ testOMP.rb
@@ -1,14 +1,19 @@
-require "formula"
+
+REPOSITORY_URL="https://github.com/Itaru7/homebrew-testOMP".freeze
+HOMEBREW_GITPULLS_VERSION="1.0".freeze
 
 class Testomp < Formula
-  homepage "https://github.com/Itaru7/homebrew-testOMP"
-  url "https://github.com/Itaru7/homebrew-testOMP/archive/v1.2.tar.gz"
+  desc "Pulls all git repository under specified paths."
+  homepage REPOSITORY_URL
+  url "#{REPOSITORY_URL}/archive/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
   sha256 "18b6a72a9f3e952c4b4a6f58eb5995d80f94dcc09d433865b38f8816d2dc1f53"
-  head "https://github.com/Itaru7/homebrew-testOMP.git"
-  version "1.2"
+  head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_GITPULLS_VERSION
 
   def install
-    system "make"
-    bin.install "testOMP"
+    bin.install "bin/git-pulls"
+  end
+
+  test do
+    system "false"
   end
 end

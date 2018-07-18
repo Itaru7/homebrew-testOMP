1b73de5429daa331d793456376506aee35b38c4e
Itaru7 <ir172178@gmail.com>
2018-07-18 14:50:43 -0700

    changed

--- testOMP.rb
+++ testOMP.rb
@@ -10,7 +10,7 @@ class Testomp < Formula
   head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_GITPULLS_VERSION
 
   def install
-    bin.install "bin/testOMP"
+    bin.install "testOMP"
   end
 
   test do
